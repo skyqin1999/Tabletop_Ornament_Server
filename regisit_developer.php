2abc5bbc5659b5bea4d7ac0dc9cf90cd8391c2fb
ZBRaphael <zhangbo19970312@gmail.com>
2021-10-10 18:28:08 +0800

    新增注册开发者页面

---
+++ regisit_developer.php
@@ -0,0 +1,26 @@
+<?php
+$servername = "8.131.65.42";
+$username = "root";
+$password = "Test123.";
+$devname = $_POST["devname"];
+
+// 创建连接
+$conn = new mysqli($servername, $username, $password);
+ 
+// 检测连接
+if ($conn->connect_error) {
+    die("连接失败: " . $conn->connect_error);
+} 
+echo "连接成功" . "<br>";
+
+$sql = "INSERT INTO `iot`.`table_developer` (`devname`) VALUES ('$devname');";
+
+if ($conn->query($sql) === TRUE) {
+    echo "新记录插入成功\n";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error . "<br>";
+}
+
+$conn->close();
+
+?>
