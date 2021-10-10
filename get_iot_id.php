0c39b31f6ee0376d7f3c35b5c8cfb7818a2c4307
ZBRaphael <zhangbo19970312@gmail.com>
2021-10-10 19:15:06 +0800

    getid返回json格式，并且加入错误码-1

--- get_iot_id.php
+++ get_iot_id.php
@@ -10,22 +10,24 @@ $conn = new mysqli($servername, $username, $password);
 if ($conn->connect_error) {
     die("连接失败: " . $conn->connect_error);
 } 
-echo "连接成功\n";
 
 $sql = "INSERT INTO `iot`.`table_iot` () VALUES ();";
 
 if ($conn->query($sql) === TRUE) {
-    echo "新记录插入成功\n";
     $sql = "SELECT LAST_INSERT_ID() as last_id";
     $result = $conn->query($sql);
  
     if ($result->num_rows > 0) {
         // 输出数据
         while($row = $result->fetch_assoc()) {
-            echo "last_id: " . $row["last_id"]. "<br>";
+            $push = array('id' => intval($row["last_id"]));
+            $push_content = json_encode($push);
+            echo $push_content;
         }
     } else {
-        echo "0 结果";
+        $push = array('id' => -1);
+        $push_content = json_encode($push);
+        echo $push_content;
     }
 } else {
     echo "Error: " . $sql . "<br>" . $conn->error."\n";
