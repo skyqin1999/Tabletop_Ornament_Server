ef5aaac10f8d2f6633e1d5b157b98ef5b6659cc2
ZBRaphael <zhangbo19970312@gmail.com>
2021-10-10 17:54:43 +0800

    增加文件上传功能

--- upload_iot_frame.php
+++ upload_iot_frame.php
@@ -3,7 +3,45 @@ $servername = "8.131.65.42";
 $username = "root";
 $password = "Test123.";
 $versionname = $_POST["versionname"];
- 
+
+// 允许上传的图片后缀
+$allowedExts = array("gif", "jpeg", "jpg", "png", "docx", "bin");
+$temp = explode(".", $_FILES["file"]["name"]);
+echo $_FILES["file"]["size"];
+$extension = end($temp);     // 获取文件后缀名
+if (($_FILES["file"]["size"] < 204800000)   // 小于 200 kb
+&& in_array($extension, $allowedExts))
+{
+    if ($_FILES["file"]["error"] > 0)
+    {
+        echo "错误：: " . $_FILES["file"]["error"] . "<br>";
+    }
+    else
+    {
+        echo "上传文件名: " . $_FILES["file"]["name"] . "<br>";
+        echo "文件类型: " . $_FILES["file"]["type"] . "<br>";
+        echo "文件大小: " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
+        echo "文件临时存储的位置: " . $_FILES["file"]["tmp_name"] . "<br>";
+        
+        // 判断当前目录下的 upload 目录是否存在该文件
+        // 如果没有 upload 目录，你需要创建它，upload 目录权限为 777
+        if (file_exists("../upload/" . $_FILES["file"]["name"]))
+        {
+            echo $_FILES["file"]["name"] . " 文件已经存在。 ";
+        }
+        else
+        {
+            // 如果 upload 目录不存在该文件则将文件上传到 upload 目录下
+            move_uploaded_file($_FILES["file"]["tmp_name"], "upload/" . $_FILES["file"]["name"]);
+            echo "文件存储在: " . "upload/" . $_FILES["file"]["name"];
+        }
+    }
+}
+else
+{
+    echo "非法的文件格式";
+}
+
 // 创建连接
 $conn = new mysqli($servername, $username, $password);
  
@@ -11,14 +49,15 @@ $conn = new mysqli($servername, $username, $password);
 if ($conn->connect_error) {
     die("连接失败: " . $conn->connect_error);
 } 
-echo "连接成功\n";
-
+echo "连接成功" . "<br>";
+$versionname = $_FILES["file"]["name"];
+echo $versionname;
 $sql = "INSERT INTO `iot`.`table_version` (`versionname`) VALUES ('$versionname');";
 
 if ($conn->query($sql) === TRUE) {
     echo "新记录插入成功\n";
 } else {
-    echo "Error: " . $sql . "<br>" . $conn->error."\n";
+    echo "Error: " . $sql . "<br>" . $conn->error . "<br>";
 }
 
 $conn->close();
